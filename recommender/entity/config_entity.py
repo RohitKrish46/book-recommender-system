579356323c56647a2b49b621941c97538c97e697
Rohit Krishnan <krishr.somu@gmail.com>
2025-05-14 17:40:35 +0530

    adding data validation data class

--- recommender/entity/config_entity.py
+++ recommender/entity/config_entity.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
-from typing import Optional
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
     kaggle_dataset_name: str
     raw_data_dir: str
     ingested_data_dir: str
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+  clean_data_dir: str
+  serialized_objects_dir: str
+  books_csv_file: str
+  ratings_csv_file: str
