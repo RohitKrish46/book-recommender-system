0f2c5bb003c5df8f802b12df354dc00b78f3db84
Rohit Krishnan <krishr.somu@gmail.com>
2025-05-15 08:36:47 +0530

    adding data class for data transformation and model training

--- recommender/entity/config_entity.py
+++ recommender/entity/config_entity.py
@@ -12,3 +12,21 @@ class DataValidationConfig:
   serialized_objects_dir: str
   books_csv_file: str
   ratings_csv_file: str
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+  clean_data_file_path: str
+  transformed_data_dir: str
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+  transformed_data_file_dir: str
+  trained_model_dir: str
+  trained_model_name: str
+
+@dataclass(frozen=True)
+class ModelRecommendationConfig:
+  book_name_serialized_objects: str
+  book_pivot_serialized_objects: str
+  final_rating_serialized_objects: str
+  trained_model_path: str
