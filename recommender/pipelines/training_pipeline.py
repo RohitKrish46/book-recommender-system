d5c3a823abbafc6a52a6bbbe585a3f7c38fa012a
Rohit Krishnan <krishr.somu@gmail.com>
2025-05-15 08:37:11 +0530

    updated training pipeline to add data transformation and model training step

--- recommender/pipelines/training_pipeline.py
+++ recommender/pipelines/training_pipeline.py
@@ -1,16 +1,23 @@
 import sys
-from recommender.components.ingest_data import DataIngestionFactory, ZipDataIngestion
+from recommender.components.data_ingestion import DataIngestionFactory, ZipDataIngestion
+from recommender.components.data_validation import DataValidation
+from recommender.components.data_transformation import DataTransformation
+from recommender.components.model_training import ModelTrainer
 from recommender.exception.exception_handler import AppException
-from recommender.logger.log import logging
+from recommender.logger import log
+import logging
+
 class TrainingPipeline:
     def __init__(self):
-        self.data_ingestion = DataIngestionFactory()
+        pass
+
 
     def start_training_pipeline(self):
         # step 1: Data Ingestion
         try:
             # get data ingestor
-            ingestor = self.data_ingestion.get_data_ingestor()
+            data_ingestion = DataIngestionFactory()
+            ingestor = data_ingestion.get_data_ingestor()
 
             # only download if file does not exist
             if isinstance(ingestor, ZipDataIngestion):
@@ -22,12 +29,36 @@ class TrainingPipeline:
         except Exception as e:
             logging.error(f"Error during ingestion: {e}")
             raise AppException(e, sys) from e
-            
-            # Step 2: Preprocess data
-            # self.data_preprocessing.preprocess_data()
-            # Step 3: Train model
-            # self.model_training.train_model()
-            # Step 4: Evaluate model
-            # self.model_evaluation.evaluate_model()
+        
+        # step 2: Data Validation / Preprocessing
+        try:      
+            # initiate data reprocess data
+            data_validation = DataValidation()
+            data_validation.start_data_validation()
+
+        except Exception as e:
+            logging.error(f"Error during data validation: {e}")
+            raise AppException(e, sys) from e
+        
+        # step 3: Data Transformation
+        try:
+            # initiate data transformation
+            data_transformation = DataTransformation()
+            data_transformation.initiate_data_transformation()
+
+        except Exception as e:
+            logging.error(f"Error during data transformation: {e}")
+            raise AppException(e, sys) from e
+        
+        # step 4: Model Training
+        try:
+            # initiate model training
+            model_trainier = ModelTrainer()
+            model_trainier.initiate_model_trainer()
+
+        except Exception as e:
+            logging.error(f"Error during model training: {e}")
+            raise AppException(e, sys) from e
+
 
             
